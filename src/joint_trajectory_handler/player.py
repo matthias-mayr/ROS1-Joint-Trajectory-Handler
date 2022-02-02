72257b36091f1bb1adc0c8b9c43406201b873b5f
Matthias Mayr <matthias.mayr@cs.lth.se>
2022-02-02 20:17:09 +0100

    New: Introduces trajectory player.

---
+++ src/joint_trajectory_handler/player.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python
+from __future__ import print_function
+
+import csv
+import glob
+import os
+import signal
+import sys
+
+import actionlib
+import rospy
+from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+
+class joint_trajectory_player:
+    def __init__(self, namespace=""):
+        rospy.init_node('joint_trajectory_player')
+        namespace = namespace.split(":=")[-1]
+        self.action_server_ns = rospy.get_param('player/action_server')
+        self.folder = rospy.get_param(namespace + "/folder")
+        self.trajectory_file = rospy.get_param(namespace + "/trajectory_file")
+
+        self.client = actionlib.SimpleActionClient(self.action_server_ns, FollowJointTrajectoryAction)
+        self.client.wait_for_server()
+
+    def handle_abort(self, signum, frame):
+        rospy.loginfo("Aborted")
+        self.client.cancel_goal()
+        exit(0)
+
+    def play(self):
+        filename = self.select_trajectory()
+        rospy.loginfo("Loading from %s.", filename)
+        traj_points = list()
+        try:
+            with open(filename, mode='r') as csv_file:
+                csv_reader = csv.reader(csv_file)
+                read_column_names = False
+                for row in csv_reader:
+                    if not read_column_names:
+                        read_column_names = True
+                        # Last column is the timestamp
+                        joints = row[:-1]
+                        continue
+                    traj_points.append(list(map(float, row)))
+        except IOError:
+            rospy.logerr("Could not open file. Aborting.")
+            exit(1)
+        rospy.loginfo("Loaded trajectory with %i joints and %i data points.", len(joints), len(traj_points))
+
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory.joint_names = joints
+        for p in traj_points:
+            point = JointTrajectoryPoint()
+            point.positions = p[:-1]
+            point.time_from_start = rospy.Duration(p[-1])
+            goal.trajectory.points.append(point)
+        rospy.loginfo("Sending to server. Trajectory length is %f seconds.", traj_points[-1][-1])
+        self.client.send_goal_and_wait(goal)
+        rospy.loginfo("Finished. Got result:\n%s", self.client.get_result())
+        return
+
+    def select_trajectory(self):
+        if self.trajectory_file:
+            return self.trajectory_file
+        if not self.folder:
+            rospy.logfatal("Either 'trajectory_file' or 'folder' needs to be specified.")
+        files = list()
+        os.chdir(self.folder)
+        for file in glob.glob("*.csv"):
+            files.append(file)
+        for it, file in enumerate(files):
+            rospy.loginfo("%i: %s", it, file)
+        response = input("Choose a file:")
+        if not response:
+            return files[-1]
+        else:
+            return files[int(response)]
+
+
+if __name__ == '__main__':
+    player = joint_trajectory_player(sys.argv[1])
+    signal.signal(signal.SIGINT, player.handle_abort)
+    player.play()
